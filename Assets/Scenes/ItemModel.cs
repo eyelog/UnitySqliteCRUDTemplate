dd48882e4d327e24eb5d5df7be1c16185b108b84
eyelog <41166734+eyelog@users.noreply.github.com>
2022-11-01 10:08:31 +0300

    main code

---
+++ Assets/Scenes/ItemModel.cs
@@ -0,0 +1,16 @@
+
+[System.Serializable]
+public struct ItemModel
+{
+    public int id { get; set; }
+    public string val { get; set; }
+
+    public ItemModel (
+        int id,
+        string val
+    ) {
+
+        this.id = id;
+        this.val = val;
+    }
+}
